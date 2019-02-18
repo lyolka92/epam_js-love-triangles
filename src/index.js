3c1a95a5b6a12c9d3b2f9889ec2d77e137231a66
Sergey Guchok <gck.sergey@gmail.com>
2019-02-18 17:25:38 +0300

    feat: implement getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let LoveTrinaglesCount = 0;
+    if (!preferences.length) {
+        return 0;
+    }
+
+    preferences.forEach((item, index) => {
+        if (preferences[preferences[item - 1] - 1] === index + 1) {
+            LoveTrinaglesCount += 1;
+        }
+    })
+
+    return Math.floor(LoveTrinaglesCount / 3);
 };
