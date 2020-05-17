9e4e54c1deda86fd8d13564a6f8ba395e3161920
Olga Serova <lyolka92@gmail.com>
2020-05-17 19:17:00 +0300

    feat: add getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-    let LoveTrinaglesCount = 0;
-    if (!preferences.length) {
-        return 0;
+    let loveTrianglesCount = 0;
+
+    if (preferences.length > 2) {
+        preferences.forEach((spichonee, index) => {
+            const firstLoverIndex = spichonee - 1,
+                nextLoverIndex = preferences[firstLoverIndex] - 1;
+            if (preferences[nextLoverIndex] - 1 === index) {
+                loveTrianglesCount += 1;
+            }
+        });
     }
 
-    preferences.forEach((item, index) => {
-        if (preferences[preferences[item - 1] - 1] === index + 1) {
-            LoveTrinaglesCount += 1;
-        }
-    })
-
-    return Math.floor(LoveTrinaglesCount / 3);
+    return Math.floor(loveTrianglesCount/3);
 };
